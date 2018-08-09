5e775d5c8f8774a101b28cf1ccb4cd80e940fc34
Kushal Srivastava <ve68mizy@stud.informatik.uni-erlangen.de>
2018-08-09 16:53:05 +0200

    changes to the load vector and np.multiply giving wrong resutls

--- ProjectCNN/jacobiCNN.py
+++ ProjectCNN/jacobiCNN.py
@@ -14,33 +14,40 @@ import matplotlib.pyplot as plt
 
 
 #jacobi iterations
-nn = 1000
+nn = 10
 
 scale_factor = - nn * nn
 
 xx = np.linspace(0, 1, nn)
 x = xx[1:nn-1]
 
-load_function = np.multiply(-x,np.exp(x), (x+3))
+load_function = 2 * np.exp(x) + x * np.exp(x) 
 #incorporating the boundary
-load_function[0] = load_function[0] - (-8) / scale_factor
-load_function[nn-3] = load_function[nn-3] - (4 + 4*np.exp(1)) / scale_factor
+boundary_x0 = 0.
+boundary_x1 = 2 * np.exp(1) + np.exp(1)
+load_function[0] = load_function[0] - boundary_x0
+load_function[nn-3] = load_function[nn-3] - boundary_x1
 
-exact_solution = 4 * x - 8 * np.exp(x) + 5 * np.multiply(x, np.exp(x)) - np.multiply(np.exp(x), x, x)
+exact_solution = 4 * x - 8 * np.exp(x) + 5 * x * np.exp(x) - np.exp(x) * x * x
 
 D = scale_factor * -2 * np.ones((nn-2, 1), float).flatten()
 E = scale_factor * np.ones((nn-3, 1), float).flatten()
 A = diags([E, np.zeros((nn-2, 1), float).flatten(), E], [-1, 0, 1]).toarray()
 
 sol2 = np.zeros(np.size(x))
-n_iter = 20000
+
+n_iter = 200
 #array for training data:
 inputData = np.zeros((n_iter, np.size(sol2)), float)
 for i in range(n_iter):
   sol2 = (load_function - np.dot(A, sol2)) / D
-  
-plt(x, sol2)
- 
+
+print(np.max(exact_solution - sol2))
+
+#plotting the results
+plt.plot(x, exact_solution, 'r')
+plt.show()
+
 
 
 
